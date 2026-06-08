96e7df283395bc78dfb15bee4400e26c28503464
guimig <82185508+guimig@users.noreply.github.com>
2026-06-08 19:50:32 -0300

    Update userEleventySetup to copy site assets
    Add passthrough copy for site assets in Eleventy setup.

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -2,9 +2,15 @@ function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
 }
+
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  eleventyConfig.addPassthroughCopy({
+    "src/site/assets": "assets"
+  });
 }
+
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
